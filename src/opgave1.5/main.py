9c04227b8ba13d6707d061041235e066ca09f471
Marnick Goossens <marnick.goossens.04@gmail.com>
2022-04-25 15:13:22 +0200

    opgave1.5

--- src/opgave1.5/main.py
+++ src/opgave1.5/main.py
@@ -1,4 +1,24 @@
 # This file should be used to show the implementation of finance.py
 # For instructions read the README.md
-import finance
-from finance import create_spend_chart
+from finance import *
+
+hobby = Grouping("Hobby")
+travel_expenses = Grouping("Travel Expenses")
+animals = Grouping("Animals")
+
+hobby.deposit(500, "Maandgeld")
+travel_expenses.deposit(1000, "Loon")
+animals.deposit(1500, "Zwart werk")
+
+hobby.withdraw(100)
+hobby.withdraw(200)
+hobby.withdraw(300)
+travel_expenses.withdraw(300, "Hollywood")
+travel_expenses.withdraw(150, "Genk")
+travel_expenses.withdraw(800, "Malaga")
+animals.withdraw(400, "Eten voor de hond en zijn puppy")
+animals.withdraw(50, "Eten voor de buurman zijn kat")
+
+print(hobby)
+print(travel_expenses)
+print(animals)
